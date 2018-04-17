0e7856de9827319e58e605b9366f1b3f8decfef0
yuwei.wang <yuwei.wang@ele.me>
2018-04-17 14:03:36 +0800

    support update view's side value

--- uetool/src/main/java/me/ele/uetool/items/EditTextItem.java
+++ uetool/src/main/java/me/ele/uetool/items/EditTextItem.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import me.ele.uetool.Element;
 
+import static me.ele.uetool.items.EditTextItem.Type.TYPE_HEIGHT;
+import static me.ele.uetool.items.EditTextItem.Type.TYPE_WIDTH;
 import static me.ele.uetool.items.EditTextItem.Type.TYPE_TEXT;
 import static me.ele.uetool.items.EditTextItem.Type.TYPE_TEXT_COLOR;
 import static me.ele.uetool.items.EditTextItem.Type.TYPE_TEXT_SIZE;
@@ -38,10 +40,14 @@ public class EditTextItem extends ElementItem {
       TYPE_TEXT,
       TYPE_TEXT_SIZE,
       TYPE_TEXT_COLOR,
+      TYPE_WIDTH,
+      TYPE_HEIGHT,
   })
   @Retention(RetentionPolicy.SOURCE) public @interface Type {
     int TYPE_TEXT = 1;
     int TYPE_TEXT_SIZE = 2;
     int TYPE_TEXT_COLOR = 3;
+    int TYPE_WIDTH = 4;
+    int TYPE_HEIGHT = 5;
   }
 }
