4a832db43b3ac26b8b65e475cbaa1a5d9b57eb7f
yuwei.wang <yuwei.wang@ele.me>
2018-04-26 15:10:44 +0800

    support show image span in textView

---
+++ app/src/main/java/me/ele/uetool/sample/VerticalImageSpan.java
@@ -0,0 +1,48 @@
+package me.ele.uetool.sample;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.text.style.ImageSpan;
+
+public class VerticalImageSpan extends ImageSpan {
+
+  public VerticalImageSpan(Drawable drawable) {
+    super(drawable);
+  }
+
+  @Override
+  public int getSize(Paint paint, CharSequence text, int start, int end,
+      Paint.FontMetricsInt fontMetricsInt) {
+    Drawable drawable = getDrawable();
+    Rect rect = drawable.getBounds();
+    if (fontMetricsInt != null) {
+      Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+      int fontHeight = fmPaint.descent - fmPaint.ascent;
+      int drHeight = rect.bottom - rect.top;
+      int centerY = fmPaint.ascent + fontHeight / 2;
+
+      fontMetricsInt.ascent = centerY - drHeight / 2;
+      fontMetricsInt.top = fontMetricsInt.ascent;
+      fontMetricsInt.bottom = centerY + drHeight / 2;
+      fontMetricsInt.descent = fontMetricsInt.bottom;
+    }
+    return rect.right;
+  }
+
+  @Override
+  public void draw(Canvas canvas, CharSequence text, int start, int end,
+      float x, int top, int y, int bottom, Paint paint) {
+
+    Drawable drawable = getDrawable();
+    canvas.save();
+    Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+    int fontHeight = fmPaint.descent - fmPaint.ascent;
+    int centerY = y + fmPaint.descent - fontHeight / 2;
+    int transY = centerY - (drawable.getBounds().bottom - drawable.getBounds().top) / 2;
+    canvas.translate(x, transY);
+    drawable.draw(canvas);
+    canvas.restore();
+  }
+}
