b39549c8a8fb4411efc462e426ff9bdbfc3f0317
zhangbaolong.001 <zhangbaolong.001@bytedance.com>
2020-08-09 10:18:39 +0800

    Revert "enhance for show viewstub xml"
    This reverts commit a0756dffd0b82c7be670a0aa7453264390ce0ec3.

--- app/src/debug/java/me/ele/uetool/sample/XmlLancet.java
+++ app/src/debug/java/me/ele/uetool/sample/XmlLancet.java
@@ -1,13 +1,13 @@
 package me.ele.uetool.sample;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewStub;
+
 import me.ele.lancet.base.Origin;
 import me.ele.lancet.base.annotations.Proxy;
 import me.ele.lancet.base.annotations.TargetClass;
-import me.ele.uetool.sample.ViewStubInfoMap.ViewStubInfo;
 
 public class XmlLancet {
 
@@ -15,7 +15,7 @@ public class XmlLancet {
     @TargetClass(value = "android.view.LayoutInflater")
     public View inflate(int resourceId, ViewGroup root) {
         View view = (View) Origin.call();
-        traverse(view, getResourceName(view, resourceId), null);
+        traverse(view, getResourceName(view, resourceId));
         return view;
     }
 
@@ -23,7 +23,7 @@ public class XmlLancet {
     @TargetClass(value = "android.view.LayoutInflater")
     public View inflate(int resourceId, ViewGroup root, boolean attachToRoot) {
         View view = (View) Origin.call();
-        traverse(view, getResourceName(view, resourceId), null);
+        traverse(view, getResourceName(view, resourceId));
         return view;
     }
 
@@ -31,39 +31,18 @@ public class XmlLancet {
     @TargetClass(value = "android.view.View")
     public static View inflate(Context context, int resourceId, ViewGroup root) {
         View view = (View) Origin.call();
-        traverse(view, getResourceName(view, resourceId), null);
+        traverse(view, getResourceName(view, resourceId));
         return view;
     }
 
-    @Proxy("inflate")
-    @TargetClass(value = "android.view.ViewStub")
-    public View inflate() {
-        View view = (View) Origin.call();
-        final ViewStubInfo viewStubInfo = ViewStubInfoMap.get(view.getId());
-        traverse(view, viewStubInfo.getViewStubInflatedXmlResourceName(), viewStubInfo.getViewStubXmlResourceName());
-        // set origin id
-        if (viewStubInfo.isOriginInflateIdValid()) {
-            view.setId(viewStubInfo.getOriginInflateId());
+    private static void traverse(View view, String name) {
+        if (view.getTag(R.id.uetool_xml) == null) {
+            view.setTag(R.id.uetool_xml, name);
         }
-        return view;
-    }
-
-    private static void traverse(View view, String xmlName, String viewStubXmlName) {
-        if (view instanceof ViewStub) {
-            final String viewStubXmlResourceName = getResourceName(view, ((ViewStub) view).getLayoutResource());
-            ViewStubInfoMap.put((ViewStub) view, xmlName, viewStubXmlResourceName);
-        } else {
-            if (view.getTag(R.id.uetool_xml) == null) {
-                view.setTag(R.id.uetool_xml, xmlName);
-            }
-            if (view.getTag(R.id.uetool_xml_view_stub) == null && viewStubXmlName != null) {
-                view.setTag(R.id.uetool_xml_view_stub, viewStubXmlName);
-            }
-            if (view instanceof ViewGroup) {
-                ViewGroup parent = (ViewGroup) view;
-                for (int i = 0; i < parent.getChildCount(); i++) {
-                    traverse(parent.getChildAt(i), xmlName, viewStubXmlName);
-                }
+        if (view instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) view;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                traverse(parent.getChildAt(i), name);
             }
         }
     }
